c749e66da338519d8f951dfb3c4d28d4a6f2b62b
bunny1v <tom7san7@gmail.com>
2025-05-24 22:44:08 +0530

    uploaded 9 files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2703854906"
+    serviceWorkerVersion: "1280715498"
   }
 });
